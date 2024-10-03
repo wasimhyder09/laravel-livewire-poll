0a48b902a5af0407a4342eea0f71e416046d9e2a
Wasim Hyder <wasimhyder09@gmail.com>
2024-10-03 22:04:33 +0500

    Added functionality to vote in polls

--- app/Livewire/Polls.php
+++ app/Livewire/Polls.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Option;
 use App\Models\Poll;
 use Livewire\Component;
 
@@ -12,5 +13,9 @@ class Polls extends Component {
   public function render() {
     $polls = Poll::with('options.votes')->latest()->get();
     return view('livewire.polls', ['polls' => $polls]);
+  }
+
+  public function vote(Option $option) {
+    $option->votes()->create();
   }
 }
